b6d8b2635c1ea7fd72621ae1ac107fda6757dc6f
Anis Abdelmoumen Djelili <adjelili@f4r11s15.clusters.42paris.fr>
2026-01-05 17:41:12 +0100

    fork/pipe

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: adjelili <adjelili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 11:13:53 by adjelili          #+#    #+#             */
-/*   Updated: 2026/01/04 15:19:10 by adjelili         ###   ########.fr       */
+/*   Updated: 2026/01/05 14:42:43 by adjelili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,33 @@
 
 int main(void)
 {
+	int	fd[2];
 	int	id;
-	int	n;
-	int	i;
-	
-	id = fork();
-	
-	if (id == 0)
-		n = 1;
-	else
-		n = 6;
-	if (id != 0)
-		();
-	for (i = n; i < n + 5; i++)
+	// fd[0] is for reading 
+	// fd[1] is for writing
+	if (pipe(fd) == -1)
 	{
-		printf("%d ", i);
-		fflush(stdout);
+		printf("error while opening the pipe\n");
+		return (1);
 	}
+	id = fork();
+
+	if (id == 0)
+	{
+		close(fd[0]);
+		int x;
+		x = 42;
+		write(fd[1], &x, sizeof(int));
+		close(fd[1]);
+	}
+	else
+	{
+		int y;
+		close(fd[1]);
+		read(fd[0], &y, sizeof(int));
+		close(fd[0]);
+		printf("got from the child process %d\n", y);
+	}
+	
 	return (0);
 }
